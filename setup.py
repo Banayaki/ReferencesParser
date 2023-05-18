4bbaccff38964ce741738fe250af18a23f6211ac
Mukhin Artem <artemmuha@gmail.com>
2023-05-18 10:23:58 +0400

    fix(build): missing requirements file

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
-    
-with open("requirements.txt", "r") as fh:
-    requirements = fh.read()
 
 setup(
     name='references_parser',
@@ -21,7 +18,14 @@ setup(
     classifiers=[
         'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Python Modules',
-    ], install_requires=requirements.split(),
+    ], 
+    install_requires=[
+        "beautifulsoup4==4.12.2",
+        "bibtexparser==1.4.0",
+        "click==8.1.3",
+        "duckpy==3.2.0",
+        "Requests==2.30.0",
+    ],
     entry_points={
         'console_scripts': [
             'references_parser=references_parser.cli:main'
